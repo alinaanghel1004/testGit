54afd6d0bf6d2f3258872dd9756e05bb092c5a36
alina <alinaanghel1004@yahoo.com>
2013-09-20 02:10:36 +0300

    test on branch

--- SegmentControl/SegmentControl/TCSegmentControl.m
+++ SegmentControl/SegmentControl/TCSegmentControl.m
@@ -24,15 +24,6 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 - (void)setNumberOfSegments:(NSInteger)nrSegments {
 
     int xBegin = 0;
